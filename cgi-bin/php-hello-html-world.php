1f86a56d5d45e5bad1d1efe60502a9e4002221a7
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-15 16:35:50 +0000

    Added a hello world program in php

---
+++ cgi-bin/php-hello-html-world.php
@@ -0,0 +1,12 @@
+<?php
+echo "<!doctype html>";
+echo "<head><title>Hello PHP World</title></head>";
+
+echo "<h1 align=center>Hello PHP World</h1>";
+echo "<hr/>";
+echo "<p>Hello world</p>";
+echo "<p>This page was generated with the PHP programming language</p>";
+echo "<p>This page was run at " . date("Y-m-d") . "</p>Your current IP address is " . $_SERVER['REMOTE_ADDR'];
+echo "</body>";
+echo "</html>";
+?>
