d870cdb9c36fa05f33c1d75a86bbf3fc5f05a38a
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-15 18:32:20 +0000

    Fixed argument error in date()

--- cgi-bin/php-hello-json-world.php
+++ cgi-bin/php-hello-json-world.php
@@ -2,7 +2,7 @@
 header("Cache-Control: no-cache");
 header("Content-type: application/json");
 
-$date = date();
+$date = date("Y-m-d");
 $address = $_SERVER['REMOTE_ADDR'];
 
 $mssg = ["message" => "Hello World from PHP!", "date" => "Today's date is $date", "ipaddress" => $address];
