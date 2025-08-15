eba81fc7a7611b589d313cba4d6f61df8255db96
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-15 23:45:42 +0000

    Used htmlspecialchars() in case of html misinterpretation

--- cgi-bin/php-environment.php
+++ cgi-bin/php-environment.php
@@ -11,13 +11,13 @@ echo "<hr/>";
 echo "<h2>Environment Variables:</h2>";
 echo "<ul>";
 foreach ($_ENV as $env_var => $value) {
-    echo "<li><strong>$env_var</strong>: $value</li>";
+    echo "<li><strong>" . htmlspecialchars($env_var) . "</strong>: " . htmlspecialchars($value) . "</li>";
 }
 echo "</ul>";
 echo "<h2>Server Variables</h2>";
 echo "<ul>";
 foreach ($_SERVER as $serv_var => $value) {
-    echo "<li><strong>$serv_var</strong>: $value</li>";
+    echo "<li><strong>" . htmlspecialchars($serv_var) . "</strong>: " . htmlspecialchars($value) . "</li>";
 }
 echo "</ul>";
 echo "</body>";
