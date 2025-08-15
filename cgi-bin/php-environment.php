5b51b54859ce889c3d1a0986d536d743e8bbcb7d
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-15 23:53:29 +0000

    Replaced $_ENV with $env_vars=getenv()

--- cgi-bin/php-environment.php
+++ cgi-bin/php-environment.php
@@ -9,13 +9,13 @@ echo "<body>";
 echo "<h1 align=center>Environment Variables</h1>";
 echo "<hr/>";
 echo "<h2>Environment Variables:</h2>";
+$env_vars = getenv();
 echo "<ul>";
-foreach ($_ENV as $env_var => $value) {
+foreach ($env_vars as $env_var => $value) {
     echo "<li><strong>" . htmlspecialchars($env_var) . "</strong>: " . htmlspecialchars($value) . "</li>";
 }
 echo "</ul>";
 echo "<h2>Server Variables</h2>";
-echo "<br> Starting something new!";
 echo "<ul>";
 foreach ($_SERVER as $serv_var => $value) {
     echo "<li><strong>" . htmlspecialchars($serv_var) . "</strong>: " . htmlspecialchars($value) . "</li>";
