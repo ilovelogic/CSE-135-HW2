37daed169e7217d8d06b31b835d5a0091ec90cc8
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-17 00:58:51 +0000

    Waiting to delete value to first check out session params

--- cgi-bin/php-destroy-cookie-session.php
+++ cgi-bin/php-destroy-cookie-session.php
@@ -8,7 +8,7 @@ $_SESSION = [];
 $params = session_get_cookie_params();
 echo "session_name(): " . session_name() . "params[path]: " . $params['path'] . "params[domain]" . $params['domain'];
 echo "params[secure]: " . $params['secure'] . "params[httponly]: " . $params['httponly'];
-setcookie("PHPSESSID", '', time-1, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+#setcookie("PHPSESSID", '', time-1, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
 
 echo "<!doctype html>";
 echo "<head>";
