58876f8c708a985dd6370c6d40baa20601f0fa7c
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-17 00:38:52 +0000

    Checking if bug is coming from $_SESSION['username'] or another part of the code

--- cgi-bin/php-destroy-cookie-session.php
+++ cgi-bin/php-destroy-cookie-session.php
@@ -12,7 +12,6 @@ echo "</head>";
 
 echo "<body>";
 echo "<h1>Session Destroyed</h1>";
-echo $_SESSION['username'] . ", is this for real?";
 echo "<a href=\"/php-cgiform.html\">Back to the CGI Form</a><br/>";
 echo "<a href=\"/cgi-bin/php-cookie-sessions-1.php\">Back to Page 1</a><br/>";
 echo "<a href=\"/cgi-bin/php-cookie-sessions-2.php\">Back to Page 2</a><br/>";
