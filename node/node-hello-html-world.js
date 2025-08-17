b4a4e12b86e02048ad1d786052975005b2b73a53
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-17 20:50:56 +0000

    Combining parts to be in one response

--- node/node-hello-html-world.js
+++ node/node-hello-html-world.js
@@ -2,11 +2,11 @@ const express = require('express'); // importing Express module
 const app = express(); // new Express application
 
 app.get('/node/node-hello-html-world.js', (request, response) => { // called when a get received at the url
-    response.send("Cache-Control: no-cache");
-    response.send("<!doctype html>");
-    response.send("<head><title>Hello HTML World</title></head>");
-    response.send("<body><h1 align=center>Hello HTML World!</h1></body>");
-    response.send("</html>");
+    response.set("Cache-Control: no-cache");
+    response.send("<!doctype html>"
+        + "<head><title>Hello HTML World</title></head>"
+        + "<body><h1 align=center>Hello HTML World!</h1></body>"
+        + "</html>");
 });
 
-app.listen(3000); // gets server up and listening on post 300
+app.listen(3000); // gets server up and listening on post 3000
