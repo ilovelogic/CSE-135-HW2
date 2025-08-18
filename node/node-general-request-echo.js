f17182de32e4ca1592734af80f3722e876348c04
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-18 22:43:54 +0000

    Handled case of null query or message body

--- node/node-general-request-echo.js
+++ node/node-general-request-echo.js
@@ -7,14 +7,20 @@ app.use(express.urlencoded({extended: true}));
 app.all('/node/node-general-request-echo.js', (request, response) => { // for any request type
     response.set("Cache-Control", "no-cache");
 
+    // in case the query string is null
+    safe_query = request.query && typeof request.query == 'object' ? request.query : {};
+
     let query_str = "<ul>";
-    for (const [key, value] in Object.entries(request.query)) {
+    for (const [key, value] in Object.entries(safe_query)) {
         query_str += "<li>" + key + ": " + value + "</li>";
     }
     query_str += "</ul>";
 
+    // in case the message body is null
+    safe_body = request.body && typeof request.body == 'object' ? request.body : {};
+    
     let body_str = "<ul>";
-    for (const [key, value] in Object.entries(request.body)) {
+    for (const [key, value] in Object.entries(safe_body)) {
         body_str += "<li>" + key + ": " + value + "</li>";
     }
     body_str += "</ul>";
