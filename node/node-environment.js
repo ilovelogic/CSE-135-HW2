1aee44e90611c10264e2f50a94d10606ba973ff0
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-18 17:32:39 +0000

    Added check for FireFox

--- node/node-environment.js
+++ node/node-environment.js
@@ -3,12 +3,17 @@ const app = express(); //
 
 app.get('/node/node-environment.js', (request, response) => {
     let headers_str = "";
-    let server_vars_str = "";
+    let env_vars_str = "";
     for (const key in request.headers) {
         headers_str += `<li>${key}: ${request.headers[key]}</li>`;
     }
+
+    // firefox detection
+    const maybe_firefox = request.headers['user-agent'] || "";
+    const uses_firefox = maybe_firefox.toLowerCase().includes('firefox');
+    const firefox_mssg = uses_firefox ? "<h3><strong>Uses Firefox</strong></h3>" : "<h3><strong>No Firefox</strong></h3>";
     for (const key in process.env) {
-        server_vars_str += `<li>${key}: ${process.env[key]}</li>`;
+        env_vars_str += `<li>${key}: ${process.env[key]}</li>`;
     }
     response.set("Cache-Control: no-cache");
     response.send("<!doctype html>"
@@ -16,8 +21,9 @@ app.get('/node/node-environment.js', (request, response) => {
         + "<body><h1 align=center>Environment Variables</h1>"
         + "<h2>HTTP Request Headers</h2>"
         + `<ul>${headers_str}</ul>`
-        + "<h2>Server Variables</h2>"
-        + `<ul>${server_vars_str}</ul>`
+        + `${firefox_mssg}<br>`
+        + "<h2>Environment Variables</h2>"
+        + `<ul>${env_vars_str}</ul>`
         + "</body></html>");
 });
 
