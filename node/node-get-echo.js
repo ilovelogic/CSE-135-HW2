00ab0ee63623428d742dcdd602120b16abd0c16a
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-18 18:34:19 +0000

    Reading from url

---
+++ node/node-get-echo.js
@@ -0,0 +1,14 @@
+const express = require('express'); // importing Express module
+const app = express(); // new Express application
+
+app.get('/node/node-get-echo.js', (request, response) => { // called when a get received at the url
+    response.set("Cache-Control: no-cache");
+    response.send("<!doctype html>"
+        + "<head><title>Echo</title></head>"
+        + `<body><h1 align=center>Hello ${request.query.username}</h1>`
+        + `Your password is ${request.query.password}`
+        + "</body></html>");
+});
+
+app.listen(3004, () => {
+    console.log('Server running on port 3004');});
