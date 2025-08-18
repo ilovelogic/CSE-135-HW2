82f55ae0e0baa6d321c84a1fe204278d285e2504
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-18 22:46:04 +0000

    Added HTTP version to html page

--- node/node-general-request-echo.js
+++ node/node-general-request-echo.js
@@ -18,7 +18,7 @@ app.all('/node/node-general-request-echo.js', (request, response) => { // for an
 
     // in case the message body is null
     safe_body = request.body && typeof request.body == 'object' ? request.body : {};
-    
+
     let body_str = "<ul>";
     for (const [key, value] in Object.entries(safe_body)) {
         body_str += "<li>" + key + ": " + value + "</li>";
@@ -28,7 +28,8 @@ app.all('/node/node-general-request-echo.js', (request, response) => { // for an
     response.send("<!doctype html>"
         + "<head><title>General Request Echo</title></head>"
         + "<body><h1 align=center>General Request Echo</h1><hr/>"
-        + "<p><strong>HTTP Protocol: </strong>" + request.protocol.toUpperCase() + "</p>"
+        + "<p><strong>HTTP Protocol: </strong>" + request.protocol.toUpperCase() 
+        + "/" + request.httpVersion + "</p>"
         + "<p><strong>HTTP Method: </strong>" + request.method.toUpperCase() + "</p>"
         + "<p><strong>Query String: </strong></p>"
         + query_str
