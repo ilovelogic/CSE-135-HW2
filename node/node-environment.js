82aca4a1452b7ad93fac0827fbf2bd7c5d83372e
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-18 17:18:48 +0000

    Added server variables

--- node/node-environment.js
+++ node/node-environment.js
@@ -2,16 +2,22 @@ const express = require('express');
 const app = express(); // 
 
 app.get('/node/node-environment.js', (request, response) => {
-    let env_var_str = "";
+    let headers_str = "";
+    let server_vars_str = "";
     for (const key in request.headers) {
-        env_var_str += `<li>${key}: ${request.headers[key]}</li>`;
+        headers_str += `<li>${key}: ${request.headers[key]}</li>`;
+    }
+    for (const key in process.env) {
+        server_vars_str += `<li>${key}: ${process.env[key]}</li>`;
     }
     response.set("Cache-Control: no-cache");
     response.send("<!doctype html>"
         + "<head><title>Environment Variables</title></head>"
         + "<body><h1 align=center>Environment Variables</h1>"
         + "<h2>HTTP Request Headers</h2>"
-        + `<ul>${env_var_str}</ul>`
+        + `<ul>${headers_str}</ul>`
+        + "<h2>Server Variables</h2>"
+        + `<ul>${server_vars_str}</ul>`
         + "</body></html>");
 });
 
