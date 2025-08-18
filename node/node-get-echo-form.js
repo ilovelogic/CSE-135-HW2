09142e96b8feafd5cd87d9a7c59c1a227fc4943b
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-18 18:13:16 +0000

    Added form for getting username and password

---
+++ node/node-get-echo-form.js
@@ -0,0 +1,16 @@
+const express = require('express'); // importing Express module
+const app = express(); // new Express application
+
+app.get('/node/node-get-echo-form.js', (request, response) => { // called when a get received at the url
+    response.set("Cache-Control: no-cache");
+    response.send("<!doctype html>"
+        + "<head><title>Basic Form</title></head>"
+        + "<body><h1 align=center>Enter details to test our GET echoing!</h1>"
+        + "<form action=\"node-get-echo.js\" method=\"get\">Username: <input type=\"text\" name=\"username\"><br>"
+        + "Password: <input type=\"password\" name=\"password\"><br>"
+        + "<input type = \"submit\" value\"Send\"><br>"
+        + "</form></body></html>");
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');}); // gets server up and listening on post 3000
