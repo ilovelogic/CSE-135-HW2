99ce5b4126a65fc78b0fba347ba42aeee45ddae2
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-18 20:06:29 +0000

    Added post echoing

--- node/node-post-echo-form.js
+++ node/node-post-echo-form.js
@@ -1,17 +1,17 @@
 const express = require('express'); // importing Express module
 const app = express(); // new Express application
 
-app.get('/node/node-get-echo-form.js', (request, response) => { // called when a get received at the url
+app.get('/node/node-post-echo-form.js', (request, response) => { // called when a get received at the url
     response.set("Cache-Control", "no-cache");
     response.send("<!doctype html>"
         + "<head><title>Basic Form</title></head>"
         + "<body><h1 align=center>Enter details to test our GET echoing!</h1>"
-        + "<form action=\"node-get-echo.js\" method=\"get\">Username: <input type=\"text\" name=\"username\"><br>"
+        + "<form action=\"node-post-echo.js\" method=\"post\">Username: <input type=\"text\" name=\"username\"><br>"
         + "Password: <input type=\"password\" name=\"password\"><br>"
         + "<input type = \"submit\" value = \"Send\"><br>"
         + "</form></body></html>");
 });
 
-app.listen(3003, () => {
-    console.log('Server running on port 3003');
+app.listen(3005, () => {
+    console.log('Server running on port 3005');
 });
---
+++ node/node-post-echo.js
@@ -0,0 +1,17 @@
+const { urlencoded } = require('body-parser');
+const express = require('express'); // importing Express module
+const app = express(); // new Express application
+
+app.use(express.urlencoded({extended: true}));
+
+app.post('/node/node-get-echo.js', (request, response) => { // called when a get received at the url
+    response.set("Cache-Control", "no-cache");
+    response.send("<!doctype html>"
+        + "<head><title>Echo</title></head>"
+        + "<body><h1 align=center>Hello " + request.body.username + "</h1>"
+        + "Your password is " + request.body.password
+        + "</body></html>");
+});
+
+app.listen(3006, () => {
+    console.log('Server running on port 3006');});
