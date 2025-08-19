ad7ebb1c1f24aa82b68173e7d90fc54689caf0a2
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-19 21:37:23 +0000

    Removed LogRocket to see if that corrects the bug

--- members/script.js
+++ members/script.js
@@ -1,6 +1,3 @@
-import LogRocket from 'logrocket';
-LogRocket.init('sv4fi3/annekelleysite');
-
 const factArray = ["Both my parents earned their Ph.D.s at UCSD.", "I know a few Latin songs by heart.", "My favorite proof is the one for the infinitude of primes.", "I have eight siblings.", "I composed a love poem about mathematics at the age of fourteen.", "I was homeschooled."];
 const colArray = ["red", "green", "blue", "purple","orange"]
 let factIndex = 0;
